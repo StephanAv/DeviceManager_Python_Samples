e578c17e0dee9a450864974a81e5314b46db6a18
StephanA <s.avenwedde@gmail.com>
2023-02-07 16:54:07 +0100

    Dev commit

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os, subprocess
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+DeviceManager = Extension(
+                    'DeviceManager',
+                    libraries = ['DeviceManager'],
+                    sources = ['my_py_module.cpp', 'my_class_py_type.cpp'],
+                    extra_compile_args=['-std=c++17']
+                    )
+
+class CustomBuild(build_ext):
+
+    def build_extensions(self):
+
+        for ext in self.extensions:
+            print("Test")
+            if ext.name == 'DeviceManager':
+                try:
+                    out = subprocess.check_output(['cmake', '--version'])
+                except Exception:
+                    raise RuntimeError('Cannot find CMake executable')
+
+                if not os.path.exists(self.build_temp):
+                    os.makedirs(self.build_temp)
+
+                cfg = 'Debug' if self.debug else 'Release'
+                
+                # Config
+                subprocess.check_call(['cmake', '-S', 'DeviceManager_ADS_Samples', '-B',  self.build_temp])
+
+                # Build
+                subprocess.check_call(['cmake', '--build', self.build_temp, '--target', 'DeviceManager', '--config', cfg])
+
+                ext.library_dirs.append(self.build_temp)
+
+            super(CustomBuild, self).build_extension(ext)
+
+
+
+setup   (  
+        ext_modules = [DeviceManager],
+        cmdclass = {'build_ext': CustomBuild}
+        )
